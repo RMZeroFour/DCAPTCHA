e2246addceca9eb201f639416917b5ab255d79d3
RMZeroFour <ritobroto04@gmail.com>
2024-09-27 01:19:11 +0530

    Update bot for new website

--- back/main.py
+++ back/main.py
@@ -1,16 +1,22 @@
 from datetime import datetime
 from os import environ
+from pymongo import MongoClient
 from starlette.requests import Request
-import model_inference as mi
+# import model_inference as mi
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-import numpy as np
-import base64
+# import numpy as np
+# import base64
 import uvicorn
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 
 captcha_digits = 3
-
+mongo_uri = 'mongodb+srv://Hemant_MongoDB_071:Hemant%40MongoDB%40071@hemant-mongodb-071.150fkzd.mongodb.net/'
+# environ.get("MONGO_URI")
+try:
+    client = MongoClient(mongo_uri)
+except Exception as e:
+    print(f"Error in connecting to the database: \n {e}")
 app = FastAPI(debug=True)
 app.add_middleware(
     CORSMiddleware,
@@ -20,53 +26,145 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
 @app.get("/")
 async def root():
     return {"message": "Hello World -20240331 " + str(datetime.now())}
 
-@app.post("/predict/")
-async def get_predictions(req: Request):
-    try :
-        data = await req.json()
-        time_taken = data['time_taken']
-        typing_speed = data['typing_speed']
-        mouse_distance = data['mouse_distance']
-        country = data['country']
-        city = data['city']
-        is_proxy = data['is_proxy']
-        result = mi.model_inference(
-            time_taken, typing_speed, mouse_distance, country, city, is_proxy)
-        if result > 0.8:
-            print(result)
-            return {"Status": "Success", "result": "Bot"}
-        elif 0.8 >= result >= 0.2:
-            print(result)
-            return {"Status": "Success", "result": "Not Sure"}
-        else:
-            print(result)
-            return {"Status": "Success", "result": "Human"}
+# @app.post("/predict/")
+# async def get_predictions(req: Request):
+#     try :
+#         data = await req.json()
+#         time_taken = data['time_taken']
+#         typing_speed = data['typing_speed']
+#         mouse_distance = data['mouse_distance']
+#         country = data['country']
+#         city = data['city']
+#         is_proxy = data['is_proxy']
+#         result = mi.model_inference(
+#             time_taken, typing_speed, mouse_distance, country, city, is_proxy)
+#         if result > 0.8:
+#             print(result)
+#             return {"Status": "Success", "result": "Bot"}
+#         elif 0.8 >= result >= 0.2:
+#             print(result)
+#             return {"Status": "Success", "result": "Not Sure"}
+#         else:
+#             print(result)
+#             return {"Status": "Success", "result": "Human"}
+#     except Exception as e:
+#         return {"Status": "Error", "message": str(e)}
+
+# @app.get("/captcha_image/")
+# async def get_captcha_image():
+#     answer = ''
+#     for _ in range(captcha_digits):
+#         img, ans = mi.get_adv_image(0.2)
+#         if _ == 0:
+
+#             image=np.array(img[0])
+#         else :
+#             image=np.hstack((image, np.array(img[0])))
+#         answer+=str(ans)
+#     plt.imsave("captcha.png", image, cmap='gray')
+#     with open("captcha.png", "rb") as png_file:
+#         png_data = png_file.read()
+#         base64_encoded = base64.b64encode(png_data).decode('utf-8')
+#     data_uri = f"data:image/png;base64,{base64_encoded}"
+#     return {
+#         "Status": "Success",
+#         "image": data_uri,
+#         "answer": answer
+#     }
+
+
+@app.post("/collect_data_layer_one/")
+async def collect_layer_1_data(req: Request):
+    data = await req.json()
+    is_abuser = data['is_abuser']
+    time_taken = data['time_taken']
+    typing_speed = data['typing_speed']
+    mouse_distance = data['mouse_distance']
+    is_proxy = data['is_proxy']
+    state = data['state']
+    country = data['country']
+    user_agent = req.headers['user-agent']
+    try:
+        database = client.training_data
+        collection = database.layer_one
+        collection.insert_one({
+            "is_abuser": is_abuser,
+            "time_taken": time_taken,
+            "typing_speed": typing_speed,
+            "mouse_distance": mouse_distance,
+            "is_proxy": is_proxy,
+            "state": state,
+            "country": country,
+            "user_agent": user_agent,
+            "is_bot": True
+        })
+        return {"Status": "Success"}
     except Exception as e:
         return {"Status": "Error", "message": str(e)}
-@app.get("/captcha_image/")
-async def get_captcha_image():
-    answer = ''
-    for _ in range(captcha_digits):
-        img, ans = mi.get_adv_image(0.2)
-        if _ == 0:
 
-            image=np.array(img[0])
-        else :
-            image=np.hstack((image, np.array(img[0])))
-        answer+=str(ans)
-    plt.imsave("captcha.png", image, cmap='gray')
-    with open("captcha.png", "rb") as png_file:
-        png_data = png_file.read()
-        base64_encoded = base64.b64encode(png_data).decode('utf-8')
-    data_uri = f"data:image/png;base64,{base64_encoded}"
-    return {
-        "Status": "Success",
-        "image": data_uri,
-        "answer": answer
-    }
+
+@app.post("/collect_data_layer_two/")
+async def collect_layer_2_data(req: Request):
+    data = await req.json()
+    is_abuser = data['is_abuser']
+    time_taken = data['time_taken']
+    mouse_distance = data['mouse_distance']
+    is_proxy = data['is_proxy']
+    state = data['state']
+    country = data['country']
+    is_solved = data['is_solved']
+    user_agent = req.headers['user-agent']
+    try:
+        database = client.training_data
+        collection = database.layer_two
+        collection.insert_one({
+            "is_abuser": is_abuser,
+            "time_taken": time_taken,
+            "mouse_distance": mouse_distance,
+            "is_proxy": is_proxy,
+            "state": state,
+            "country": country,
+            "is_solved": is_solved,
+            "user_agent": user_agent,
+            "is_bot": True
+        })
+        return {"Status": "Success"}
+    except Exception as e:
+        return {"Status": "Error", "message": str(e)}
+
+
+@app.post("/collect_data_layer_three/")
+async def collect_layer_3_data(req: Request):
+    data = await req.json()
+    is_abuser = data['is_abuser']
+    time_taken = data['time_taken']
+    mouse_distance = data['mouse_distance']
+    is_proxy = data['is_proxy']
+    state = data['state']
+    country = data['country']
+    problem_solved = data['problem_solved']
+    user_agent = req.headers['user-agent']
+    try:
+        database = client.training_data
+        collection = database.layer_three
+        collection.insert_one({
+            'is_abuser': is_abuser,
+            'time_taken': time_taken,
+            'mouse_distance': mouse_distance,
+            'is_proxy': is_proxy,
+            'state': state,
+            'country': country,
+            'problem_solved': problem_solved,
+            'user_agent': user_agent,
+            "is_bot": True
+        })
+        return {"Status": "Success"}
+    except Exception as e:
+        return {"Status": "Error", "message": str(e)}
 if __name__ == "__main__":
     uvicorn.run(app)
--- bot/bot.py
+++ bot/bot.py
@@ -1,48 +1,126 @@
 from sys import argv
 from time import sleep
+from argparse import ArgumentParser, BooleanOptionalAction
 from random import random, randrange
 from selenium import webdriver
 from selenium.webdriver import ActionChains
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
+
+parser = ArgumentParser()
+parser.add_argument('--mouse', action=BooleanOptionalAction)
+parser.add_argument('--iterations', type=int)
+parser.add_argument('--chrome')
+parser.add_argument('--chromedriver')
+parser.add_argument('--frontend')
+args = parser.parse_args()
+
+def random_sleep(low, high):
+    sleep(random() * (high - low) + low)
+
+def random_mouse_pos(actions, low_x, low_y, high_x, high_y, ref_element):
+    if args.mouse:
+        mouse_offset_x = randrange(low_x, high_x)
+        mouse_offset_y = randrange(low_y, high_y)
+        actions.move_to_element_with_offset(ref_element, mouse_offset_x, mouse_offset_y).perform()
+
+def home_page(driver, actions):
+    random_mouse_pos(actions, -10, -5, 10, 5, driver.find_element(By.CSS_SELECTOR, "a[class*='loginBtn']"))
+    actions.click().perform()
+
+def aadhar_entry(driver, actions):
+    random_mouse_pos(actions, -10, -5, 10, 5, driver.find_element(By.CSS_SELECTOR, "input[class*='aadharInp']"))
+    actions.click().perform()
+    for _ in range(randrange(10, 14)):
+        actions.send_keys(randrange(0, 10)).perform()
+        random_sleep(0, 1)
+    random_mouse_pos(actions, -10, -5, 10, 5, driver.find_element(By.CSS_SELECTOR, "button[class*='submitBtn']"))
+    actions.click().perform()
+
+def numeric_captcha(driver, actions):
+    while random() < 0.50:
+        # Incorrect
+        for _ in range(3):
+            r = randrange(1, 11)
+            r = 11 if r == 10 else r
+            random_mouse_pos(actions, -10, -5, 10, 5, driver.find_element(By.CSS_SELECTOR, f"button[class*='numberBtn']:nth-of-type({r})"))
+            actions.click().perform()
+        random_mouse_pos(actions, -10, -5, 10, 5, driver.find_element(By.CSS_SELECTOR, "button[class*='numberBtn']:nth-of-type(12)"))
+        actions.click().perform()
+
+    # Correct
+    random_mouse_pos(actions, -10, -5, 10, 5, driver.find_element(By.CSS_SELECTOR, "button[class*='numberBtn']:nth-of-type(4)"))
+    actions.click().perform()
+    random_mouse_pos(actions, -10, -5, 10, 5, driver.find_element(By.CSS_SELECTOR, "button[class*='numberBtn']:nth-of-type(7)"))
+    actions.click().perform()
+    random_mouse_pos(actions, -10, -5, 10, 5, driver.find_element(By.CSS_SELECTOR, "button[class*='numberBtn']:nth-of-type(8)"))
+    actions.click().perform()
+    random_mouse_pos(actions, -10, -5, 10, 5, driver.find_element(By.CSS_SELECTOR, "button[class*='numberBtn']:nth-of-type(12)"))
+    actions.click().perform()
+
+def solve_task(driver, actions):
+    caption = driver.find_element(By.CSS_SELECTOR, "p[class*='instructionText']").get_attribute('innerText')
+    if 'order' in caption:
+        buttons = {}
+        for r in range(1, 9):
+            b = driver.find_element(By.CSS_SELECTOR, f"button:nth-of-type({r})").get_attribute('innerText')
+            buttons[int(b)] = r
+        for r in range(1, 9):
+            random_mouse_pos(actions, -5, -5, 5, 5, driver.find_element(By.CSS_SELECTOR, f"button:nth-of-type({buttons[r]})"))
+            actions.click().perform()
+    elif 'white' in caption:
+        while True:
+            try:
+                random_mouse_pos(actions, -5, -5, 5, 5, driver.find_element(By.CSS_SELECTOR, "button[class*='onTileDiv']"))
+                actions.click().perform()
+            except NoSuchElementException:
+                break
+    else:
+        random_mouse_pos(actions, -5, -5, 5, 5, driver.find_element(By.CSS_SELECTOR, "div[class*='leftDiv'] button[class*='squareDiv']"))
+        actions.click().perform()
+        random_mouse_pos(actions, -5, -5, 5, 5, driver.find_element(By.CSS_SELECTOR, "div[class*='rightDiv'] button[class*='squareDiv']"))
+        actions.click().perform()
+
+        random_mouse_pos(actions, -5, -5, 5, 5, driver.find_element(By.CSS_SELECTOR, "div[class*='leftDiv'] button[class*='circleDiv']"))
+        actions.click().perform()
+        random_mouse_pos(actions, -5, -5, 5, 5, driver.find_element(By.CSS_SELECTOR, "div[class*='rightDiv'] button[class*='circleDiv']"))
+        actions.click().perform()
+
+        random_mouse_pos(actions, -5, -5, 5, 5, driver.find_element(By.CSS_SELECTOR, "div[class*='leftDiv'] button[class*='starDiv']"))
+        actions.click().perform()
+        random_mouse_pos(actions, -5, -5, 5, 5, driver.find_element(By.CSS_SELECTOR, "div[class*='rightDiv'] button[class*='starDiv']"))
+        actions.click().perform()
 
 def main():
-    useMouse = len(argv) > 1
-
     chrome_options = webdriver.ChromeOptions()
+    chrome_options.binary_location = args.chrome
     chrome_options.add_argument("--incognito")
     chrome_options.add_argument("--start-maximized")
-    chrome_service = Service('./chromedriver')
+
+    chrome_service = Service(args.chromedriver)
     driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
+
     actions = ActionChains(driver)
 
-    while True:
-        driver.get('https://sih-ui-xi.vercel.app/')
+    for _ in range(args.iterations):
+        driver.get(args.frontend)
 
-        if useMouse:
-            mouse_offset_x = randrange(-300, 300)
-            mouse_offset_y = randrange(-150, 150)
-            html_root = driver.find_element(By.TAG_NAME, 'html')
-            actions.move_to_element_with_offset(html_root, mouse_offset_x, mouse_offset_y).perform()
-
-        sleep(random() * 2 + 1)
-
-        aadhar_input = driver.find_element(By.CLASS_NAME, 'place')
-        if useMouse:
-            actions.click(aadhar_input).perform()
-        for i in range(randrange(7, 14)):
-            aadhar_input.send_keys(randrange(0, 10))
-            sleep(randrange(0, 4) // 2)
-
-        verify_button = driver.find_element(By.CLASS_NAME, 'submit-button')
-        if useMouse:
-            offset_x = randrange(-30, 30)
-            offset_y = randrange(-10, 10)
-            actions.move_to_element_with_offset(verify_button, offset_x, offset_y).click().perform()
-        else:
-            verify_button.click()
-
-        sleep(random() * 3 + 3)
+        random_mouse_pos(actions, -300, -150, 300, 150, driver.find_element(By.TAG_NAME, 'html'))
+        
+        random_sleep(0, 2)
+        home_page(driver, actions)
+        
+        random_sleep(1, 3)
+        aadhar_entry(driver, actions)
+        
+        random_sleep(1, 3)
+        numeric_captcha(driver, actions)
+        
+        random_sleep(1, 3)
+        solve_task(driver, actions)
+        
+        random_sleep(3, 5)
 
     driver.close()
     driver.quit()
